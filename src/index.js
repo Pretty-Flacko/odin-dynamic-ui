a05be5266c034dd97228adc1c48778d04b11edfc
Pretty-Flacko <48690875+Pretty-Flacko@users.noreply.github.com>
2025-12-29 00:55:39 +0400

    Add initial dropdown menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./style.css";
+
+function initDropdown(dropdown) {
+	const toggle = dropdown.querySelector(".dropdown__toggle");
+
+	toggle.addEventListener("click", (e) => {
+		e.stopPropagation();
+		dropdown.classList.toggle("is-open");
+	});
+}
+
+document.querySelectorAll("[data-dropdown]").forEach(initDropdown);
